e15b15adc285c11fd3cb58894d161d7ca3f5bff1
안정후 <austinan@naver.com>
2024-12-22 11:59:19 +0900

    feat: testfixtures 모듈 분리

---
+++ common/src/testFixtures/java/com/drinkhere/common/TestSpringBootApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.drinkhere.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+@SpringBootApplication
+public class TestSpringBootApplicationConfig {
+}
